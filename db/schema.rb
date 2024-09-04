02cf96aac7dd17f9d06de4f6257d61882c342e7a
Olli <olli17@gmx.net>
2024-09-04 18:44:44 +0200

    update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_08_17_145133) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_04_111946) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -46,7 +46,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_08_17_145133) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "title"
-    t.string "content"
+    t.text "content"
     t.string "author"
     t.datetime "published"
     t.datetime "transcribed_at"
