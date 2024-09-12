a142aeb08828e81222bd17d03278b22bce43b15b
Olli <olli17@gmx.net>
2024-09-12 12:47:51 +0200

    setup solid_queue remove resque

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 web: bin/rails server -e production
-resque: RAILS_ENV=production QUEUE=default COUNT=2 bin/rake resque:workers
+solid_queue: bin/jobs
--- app/jobs/get_episodes_job.rb
+++ app/jobs/get_episodes_job.rb
@@ -2,6 +2,6 @@
 class GetEpisodesJob < ApplicationJob
   queue_as :fetch
   def perform(episodes,download_again = false)
-    #PodcastActions::Sync.sync_episodes(episodes)
+    PodcastActions::Sync.sync_episodes(episodes)
   end
 end
