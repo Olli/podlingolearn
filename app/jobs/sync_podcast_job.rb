a2937066c5395cf4a91a1608d923e7f6cbbb686c
Olli <oli@blue.mchnet.loc>
2025-04-07 17:01:10 +0200

    ActionCable inserted

--- app/jobs/sync_podcast_job.rb
+++ app/jobs/sync_podcast_job.rb
@@ -4,4 +4,10 @@ class SyncPodcastJob < ApplicationJob
   def perform(podcast)
     PodcastActions::Sync.sync_podcast(podcast)
   end
+
+  # Sends a Turbo Stream update to the "sync-link" target with the result text
+  def after_perform(podcast)
+    Turbo::StreamsChannel.broadcast_update_to("sync-link", {
+      result: "Sync successful!" })
+  end
 end
