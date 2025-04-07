c4e93b3ba44bc917f053794598299242f572f9fa
Olli <oli@blue.mchnet.loc>
2025-04-07 17:01:10 +0200

    ActionCable inserted

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,14 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :current_user
+    def connect
+      self.current_user = find_verified_user
+    end
+    private
+    def find_verified_user
+      return current_user if current_user && current_user.verified?
+
+      reject_unauthorized_connection
+    end
   end
 end
