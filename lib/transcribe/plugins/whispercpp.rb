ced9a110a765e26dce596b19e7cc08153b9be122
Olli <oli@blue.mchnet.loc>
2025-04-29 13:42:38 +0200

    rewrite of whispercpp plugin and bring speech2test to work again

--- lib/transcribe/plugins/whispercpp.rb
+++ lib/transcribe/plugins/whispercpp.rb
@@ -9,34 +9,45 @@ module Transcribe::Plugins
     end
     def initialize(config = ActiveSupport::OrderedOptions.new)
       @config = config
-      @config.model ||= "tiny"
       setup
     end
 
-    def transcribe!(audiofile)
-      # @whisper.transcribe(audiofile, @config.params)
-      url = URI("http://#{@config.params.http_host}:#{@config.params.http_port}/inference")
-      http = Net::HTTP.new(url.host, url.port)
-      request = Net::HTTP::Post.new(url)
-      request["Accept"] = @config.params.format_mime
-      form_data = [ [ "file",
-        File.open(audiofile) ], [ "response_format", @config.params.response_format ] ]
-      request.set_form form_data, "multipart/form-data"
-      response = http.request(request)
-      response.read_body
+    # transcribe the audio file
+    # @params [String, #read] estring with full path
+    # @returns the return code of system
+    def transcribe!(audiofile_path)
+      command = %w(
+        @config.params.binary
+        "--output-#{@config.params.output_format}"
+        "--language auto"
+        "--model #{@config.params.model_path + "/" + @config.params.model}"
+        "--output-file #{output_file_path(audiofile_path)}"
+        audiofile_path )
+      # execute the commandline on whispercpp
+      if system(command.join(" "))
+
+        "#{output_file_path(audio_file_path)}.#{@config.params.output_format}"
+      else
+        raise TranscribeError
+      end
     end
 
 
 
     private
+    # remove the file extension from the input file
+    def output_file_path(audiofile)
+      path = File.dirname(audiofile)
+      filename_without_ext = File.basename(audiofile, ".*")
+      File.join(path, filename_without_ext)
+    end
+    # sets up all the parameters for the whispercpp cli
     def setup
-      # @whisper = Whisper::Context.new(@config.model)
-      # @config.params = Whisper::Params.new
       @config.params = ActiveSupport::OrderedOptions.new
-      @config.params.http_host = "localhost"
-      @config.params.http_port = "8080"
-      @config.params.format_mime = "application/srt"
-      @config.params.response_format = "vtt"
+      @config.params.binary =  "vendor/whisper.cpp/build/bin/whisper-cli"
+      @config.params.model = "ggml-tiny.en.bin"
+      @config.params.output_format = "vtt"
+      @config.params.model_path = "vendor/whisper.cpp/models"
     end
   end
 end
