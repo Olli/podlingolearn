3dcd36bae2ac792d48fc5c7cc10d31702c4b6b7d
Olli <oli@blue.mchnet.loc>
2025-04-29 13:42:00 +0200

    srt_file is now subtitle_file

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -1,7 +1,7 @@
 class Episode < ApplicationRecord
   belongs_to :podcast
   has_one_attached :audio_file
-  has_one_attached :srt_file
+  has_one_attached :subtitle_file
 
   validates_uniqueness_of :url, scope: [ :podcast_id ]
 end
