b5924642d32efda05c0e0c48f5d7afcf22aff289
Olli <oli@blue.mchnet.loc>
2025-04-29 13:42:00 +0200

    srt_file is now subtitle_file

--- app/jobs/transcribe_job.rb
+++ app/jobs/transcribe_job.rb
@@ -4,25 +4,24 @@ class TranscribeJob < ApplicationJob
   def perform(episode)
     file = episode.audio_file
     return if file.blank?
-    srtfile = nil
+    outputfile = nil
     episode.audio_file.blob.open do |tmpfile|
-      srtfile = audio2text_process(tmpfile)
+      outputfile = audio2text_process(tmpfile)
     end
-    if srtfile
-      episode.srt_file.attach(io: srtfile,
-                              filename: episode.title + ".srt",
-                              content_type: "text/srt")
-      episode.transcribed_at = DateTime.now
-      episode.save
-    else
-      false
-    end
+
+    return if not outputfile
+
+    episode.subtitle_file.attach(io: srtfile,
+                            filename: episode.title + ".srt",
+                            content_type: "text/srt")
+    episode.transcribed_at = DateTime.now
+    episode.save
   end
 
 
   private
   def audio2text_process(tmpfile)
-    transscriber = Speech2Text.new
-    transscriber.transscribe!(tmpfile)
+    transscriber = Transcribe::Speech2Text.new
+    transscriber.transcribe(tmpfile.path)
   end
 end
