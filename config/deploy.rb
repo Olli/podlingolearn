6fda31758d3fb4d5a10620f1fe69a7704dd9c1d7
Olli <oli@blue.mchnet.loc>
2025-05-02 15:18:56 +0200

    deploy files aus master raus

---
+++ config/deploy.rb
@@ -0,0 +1,85 @@
+  ~/devel/rails-podcast-transcribe
+   .git
+   .github
+  󰉋 .kamal
+  󰉋 .ruby-lsp
+  󰉋 app
+  󱧺 bin
+   config
+  │  credentials
+      (1 hidden item)
+  │ 󰉋 deploy
+  │  environments
+  │ │ 󰴭 development.rb
+  │ │ 󰴭 production.rb                   󰋼 
+  │ └ 󰴭 test.rb                         󰋼 
+  │  initializers
+  │ │ 󰴭 assets.rb
+  │ │ 󰴭 content_security_policy.rb
+  │ │ 󰴭 devise.rb
+  │ │ 󰴭 filter_parameter_logging.rb
+  │ │ 󰴭 inflections.rb
+  │ │ 󰴭 new_framework_defaults_8_0.rb
+  │ └ 󰴭 permissions_policy.rb
+  │  locales
+  │ │ 󰉋 app
+  │ └ 󰉋 devise
+  │ 󰴭 application.rb
+  │ 󰴭 boot.rb
+  │  cable.yml
+  │ 󰈔 credentials.yml.enc
+  │  database.yml
+  │  deploy.yml
+  │ 󰴭 environment.rb
+  │ 󰴭 importmap.rb
+  │ 󰆼 init.sql
+  │ 󰴭 puma.rb
+  │  queue.yml
+  │  recurring.yml
+  │ 󰴭 routes.rb
+  └  storage.yml
+    (1 hidden item)
+   db
+  │ 󰉋 migrate
+  │ 󰴭 cable_schema.rb
+  │ 󰴭 queue_schema.rb
+  │ 󰴭 schema.rb
+  └ 󰴭 seeds.rb                          󰋼 
+  󰉋 deploy
+   lib
+  │ 󰉋 assets
+  │ 󰉋 capistrano
+  │ 󰉋 podcast_actions
+  │ 󰉋 tasks
+  └  transcribe
+    │ 󰉋 plugins
+    │ 󰴭 plugin_base.rb
+    │ 󰴭 plugin_registry.rb
+    │ 󰴭 speech_2_text.rb
+    └ 󰴭 transcribe_error.rb
+  󰉋 log
+  󰉋 public
+  󰉋 storage
+  󱞊 test
+  󰪺 tmp
+  󰉋 vendor
+  󰈔 .dockerignore
+  󰊢 .gitattributes
+  󰊢 .gitignore
+  󰒓 .gitmodules
+   .rubocop.yml
+  󰈔 .ruby-version
+  󰈔 Capfile
+  󰡨 Dockerfile
+  󰡨 Dockerfile.dev
+  󰡨 Dockerfile.whispercpp
+  󰴭 Gemfile
+  󰈔 Gemfile.lock
+  󰈔 Procfile
+  󰈔 Procfile.dev
+   README.md
+  󰴭 Rakefile
+  󰴭 config.ru
+   docker-compose-dev.yml
+   docker-compose.yml
+  (1 hidden item)
