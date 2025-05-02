e0f6ac9b2eb816c2d481cb81409666a864db5640
Olli <oli@blue.mchnet.loc>
2025-05-02 12:30:34 +0200

    seeds for initial user

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,6 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+#
+User.create!(email: "admin@admin.local", password: "admin#1234!",
+password_confirmation: "admin#1234!")
